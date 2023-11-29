0ff2b6cbed12769d310bc22709b8d20ed47d7400
lopezrbn <lopezrbn@gmail.com>
2023-11-29 10:12:46 +0100

    Reestructuring ignored files.

--- words/db_connection.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-db = {
-    "user": "ubuntu" if socket.gethostname() == "vps-955fe093" else "root",
-    "host": "localhost",
-    "database": "words_game",
-    "password": "tartadequeso"
-}
