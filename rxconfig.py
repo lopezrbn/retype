c14565b0cd4ae7d605c7a5dab824d987c203013b
lopezrbn <lopezrbn@gmail.com>
2023-12-02 03:12:02 +0100

    Modification of rxconfig.py to change IP for domain.

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,6 @@ import socket
 
 config = rx.Config(
     app_name="words",
-    api_url=("http://152.228.134.180" if socket.gethostname() == "vps-955fe093" else "http://localhost:8000"),
+    api_url=("https://rety.pe" if socket.gethostname() == "vps-955fe093" else "http://localhost:8000"),
     # env=(rx.Env.PROD if socket.gethostname() == "vps-955fe093" else rx.Env.DEV),
 )
