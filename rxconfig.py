6d0a744fee45e190813f30ea01d8cf8be4373476
lopezrbn <lopezrbn@gmail.com>
2023-12-13 12:14:29 +0100

    Removed all .gitignore files and folders.

--- rxconfig.py
+++
@@ -1,7 +0,0 @@
-import reflex as rx
-import socket
-
-config = rx.Config(
-    app_name="words",
-    api_url=("https://your-domain.com" if socket.gethostname() == "your-hostname" else "http://localhost:8000"),
-)
