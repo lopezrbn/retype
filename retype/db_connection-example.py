5667a4d808b718d1036ae97855830eb9296aac2c
Javi López <javierlopezgonzalez@gmail.com>
2024-05-19 18:13:29 +0200

    Create files as examples

---
+++ retype/db_connection-example.py
@@ -0,0 +1,6 @@
+db = {
+    "user": "root",           # default "root"
+    "host": "localhost",           # default "localhost". It can also be your host server ip.
+    "database": "retype",       # default "retype"
+    "password": "your_password"    # your password
+}
